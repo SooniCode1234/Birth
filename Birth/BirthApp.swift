6627afb6e9903d037281fa994149485177c7ac2c
SooniCode1234 <sooni.mohammed@gmail.com>
2021-10-24 10:32:27 -0400

    Initial Commit

---
+++ Birth/BirthApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthApp.swift
+//  Birth
+//
+//  Created by Sooni Mohammed on 2021-10-24.
+//
+
+import SwiftUI
+
+@main
+struct BirthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
