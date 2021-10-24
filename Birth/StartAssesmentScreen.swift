f0a4750a6d62c1d45167e68dddbf6fb4c831cd2f
SooniCode1234 <sooni.mohammed@gmail.com>
2021-10-24 11:51:25 -0400

    Added the assets and made a new file for the first screen

---
+++ Birth/StartAssesmentScreen.swift
@@ -0,0 +1,20 @@
+//
+//  StartAssesmentScreen.swift
+//  Birth
+//
+//  Created by Sooni Mohammed on 2021-10-24.
+//
+
+import SwiftUI
+
+struct StartAssesmentScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StartAssesmentScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        StartAssesmentScreen()
+    }
+}
